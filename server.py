53d3173805eccfaca0fbd0e277a460fbd539e010
Hackbright Student <info@hackbrightacademy.com>
2016-08-04 17:57:42 -0700

    Completed Part 2

--- server.py
+++ server.py
@@ -25,6 +25,12 @@ def index():
 
     return render_template("homepage.html")
 
+@app.route('/logout')
+def logged_out():
+
+    del session["user"]
+    return redirect('/')
+
 
 @app.route('/users')
 def user_list():
@@ -46,43 +52,64 @@ def register_process():
     """Redirects to homepage after submission for registered users."""
 
     email = request.form.get("email")
+    print "EMAIL ", email
     password = request.form.get("password")
 
-    db_user = User.query.filter(email == email).first()
-    db_password = User.query.filter(password == password).first()
+    user = User.query.filter(User.email == email).first()
+    print "USER ", user
 
-    if db_user.email == email:
-        
-        if db_password == password:
-            session["user"] = email #keeps track of who logged in
-            flash("successfully logged in")
-            return redirect("/")
-        
-        else:
-            flash("Password is incorrect.")
-            return redirect('/register')
-
-    else:
+    if user == None:
         user = User(email=email, password=password)
+        print "HERE"
         db.session.add(user)
         db.session.commit()
+        session["user"] = email
+        return redirect("/")
 
-    return redirect("/")
+    elif User.password == password:
+        session["user"] = email #keeps track of who logged in
+        flash("Successfully logged in.")
+        return redirect("/")
 
-    render_template("base.html")
+    else:
+        flash("Password is incorrect.")
+        return redirect('/register')
 
-# @app.route("/logged_out")
-# def logged_out_process():
+    return render_template("base.html")
+
 
 @app.route('/user_details')
 def user_details():
     """Show details of a user."""
 
-    active_user = session["user"] 
+    email = session["user"]
 
-    user=User.query.filter(email = active_user).first()
+    #filter by email in User class, not DB
+    #list of all movies user has rated
+    user_info = User.query.filter(User.email == email).first()
+    user_id = user_info.user_id
+    user_ratings = Rating.query.filter(Rating.user_id == user_id).all()
 
-    return render_template("user_details.html")
+    return render_template("user_details.html", user_ratings=user_ratings)
+
+
+@app.route('/movies')
+def movie_title_list():
+    """Show movie titles"""
+
+    movies_in_my_DB = Movie.query.order_by(Movie.title).all() #binding query to a variable
+    return render_template("movie_list.html", movies=movies_in_my_DB)
+
+
+@app.route('/movie_ratings/<int:movieid>')
+def get_movie_ratings(movieid):
+    """Show all ratings for the selected movie."""
+
+    scores = Rating.query.filter(Rating.movie_id == movieid).all()
+    title = Movie.query.get(movieid).title
+
+    return render_template("movie_detail_ratings.html", scores=scores, title=title)
+
 
 
 if __name__ == "__main__":
